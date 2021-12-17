6d9759c871c7a18fe523d57a7971795311962cc8
Samir Fernandes <79095161+samirfernandes26@users.noreply.github.com>
2021-12-17 10:07:16 -0300

    Inicio da tela de bem vindo

--- src/pages/BemVindo/index.tsx
+++ src/pages/BemVindo/index.tsx
@@ -1,22 +1,39 @@
 import React, { useEffect, useState } from 'react';
 
-import { Container, Header, Body, } from './styles';
+import { 
+    Container, 
+    Header,
+    Baseboard, 
+    Illustration,
+    Butt,
+    TextButt
+} from './styles';
+
+import watering  from '../../assets/watering.png'
 
 const BemVindo: React.FC = () => {
     
     return (
         <Container>
-
             <Header>
-
-                
+                Gerencie 
+                suas plantas deil
+                forma fácil
             </Header>
 
-            <Body>
-                
-
+            <Illustration source={watering}/>
             
-            </Body>
+            <Baseboard>
+            Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+            sempre que precisar.
+            </Baseboard>
+
+            <Butt>
+                <TextButt>
+                    Z
+                </TextButt>
+
+            </Butt>
             
         </Container>
     )
--- src/pages/BemVindo/styles.ts
+++ src/pages/BemVindo/styles.ts
@@ -1,16 +1,37 @@
-
 import styled from 'styled-components/native';
+import colors from '../../styles/colors';
 import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View`
-
-   
-`;
-
-export const Header = styled.View`
+export const Container = styled.SafeAreaView`
+    flex: 1;
+    justify-content: space-around;
+    align-items: center;
     
 `;
 
-export const Body = styled.View`
-    
+
+export const Header = styled.Text`
+  font-size: 32;
+  font-weight: bold;
+  text-align: center;
+  margin-top: 38;
+  color: ${colors.heading};
+
+`;
+
+export const Baseboard = styled.Text`
+    font-size: 18;
+    text-align: center;
+    color: ${colors.heading};
+
+`;
+
+export const Illustration = styled.Image`
+
+`
+export const Butt = styled.TouchableOpacity`
+
+`
+export const TextButt = styled.Text`
+
 `;
